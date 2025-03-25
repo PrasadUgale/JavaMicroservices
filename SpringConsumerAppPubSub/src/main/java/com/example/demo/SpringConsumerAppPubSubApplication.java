57d242ea704a6ac515e7d85a948fbccb9ede5053
Prasad Ugale <prasadugale123@gmail.com>
2025-03-25 23:51:33 +0530

    ActiveMq for Pub Sub Model

---
+++ SpringConsumerAppPubSub/src/main/java/com/example/demo/SpringConsumerAppPubSubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsumerAppPubSubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsumerAppPubSubApplication.class, args);
+	}
+
+}
